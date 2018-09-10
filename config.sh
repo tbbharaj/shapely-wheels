b3402b8f4dd75cce1647dc77aabad73f4f100423
Sean C. Gillies <sean@mapbox.com>
2018-09-10 16:54:10 -0600

    Shapely wheel builder based on rasterio-wheels

---
+++ config.sh
@@ -0,0 +1,32 @@
+# Custom utilities for Rasterio wheels.
+#
+# Test for OSX with [ -n "$IS_OSX" ].
+
+
+function build_geos {
+    build_simple geos $GEOS_VERSION https://download.osgeo.org/geos tar.bz2
+}
+
+
+function pre_build {
+    start_spinner
+    suppress build_geos
+    stop_spinner
+}
+
+
+function run_tests {
+    if [ -n "$IS_OSX" ]; then
+        export PATH=$PATH:${BUILD_PREFIX}/bin
+        export LC_ALL=en_US.UTF-8
+        export LANG=en_US.UTF-8
+    else
+        export LC_ALL=C.UTF-8
+        export LANG=C.UTF-8
+    fi
+    cd ../Shapely
+    mkdir -p /tmp/Shapely
+    cp -R tests /tmp/Shapely
+    cd /tmp/Shapely
+    python -m pytest -vv tests
+}
---
+++ env_vars.sh
@@ -0,0 +1,3 @@
+GEOS_VERSION=3.6.2
+export MACOSX_DEPLOYMENT_TARGET=10.9
+export GEOS_CONFIG=/usr/local/bin/geos-config
